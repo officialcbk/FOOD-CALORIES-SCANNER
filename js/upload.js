bad6c0740a6de2dbe3f8e40259e176753ba490bb
officialcbk <eeze@academic.rrc.ca>
2025-12-25 04:50:03 -0600

    logic in progress

---
+++ js/upload.js
@@ -0,0 +1,24 @@
+//references to DOM elements
+const imageInput = document.getElementById("imageInput");
+const previewImage = document.querySelector(".image-preview img");
+const analyzeButton = document.querySelector(".primary-btn");
+
+//listener to change
+imageInput.addEventListener("change", handleImageUpload);
+
+function handleImageUpload(event) {
+  const file = event.target.files[0];
+
+  if (!file) {
+    return;
+  }
+
+  const reader = new FileReader();
+
+  reader.onload = function () {
+    previewImage.src = reader.result;
+    analyzeButton.disabled = false;
+  };
+
+  reader.readAsDataURL(file);
+}
